b2727a7fcb08c61008e66a34d2a4e1cccd0a294a
SunriseFox <sunrisefox@qq.com>
2019-12-09 18:26:19 +0800

    chore: backup database

--- src/extension/options-page/Home.tsx
+++ src/extension/options-page/Home.tsx
@@ -231,9 +231,9 @@ export default function DashboardHomePage() {
                                     variant="contained"
                                     color="primary"
                                     className={classes.button}
-                                    component={Link}
-                                    onClick={exportData}
-                                    to="database/backup">
+                                    component={'a'}
+                                    loading={exportLoading}
+                                    onClick={exportData}>
                                     Backup
                                 </ActionButton>
                             </ListItemSecondaryAction>
